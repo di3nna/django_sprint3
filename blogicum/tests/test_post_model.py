5babadfee465da2b9419aa79e8586903fe78a260
di3nna <daiankayukova@yandex.ru>
2026-04-26 21:09:06 +0500

    Update test_post_model.py

--- blogicum/tests/test_post_model.py
+++ blogicum/tests/test_post_model.py
@@ -15,12 +15,24 @@ pytestmark = [
     ('field', 'type', 'params'), [
         ('title', CharField, {'max_length': 256}),
         ('text', TextField, {}),
-        ('pub_date', DateTimeField, {'auto_now': False, 'auto_now_add': False}),
+        ('pub_date', DateTimeField, {
+            'auto_now': False,
+            'auto_now_add': False,
+        }),
         ('author', ForeignKey, {'null': False}),
-        ('location', ForeignKey, {'null': True}),
-        ('category', ForeignKey, {'null': True, 'blank': False}),
-        ('is_published', BooleanField, {'default': True}),
-        ('created_at', DateTimeField, {'auto_now_add': True}),
+        ('location', ForeignKey, {
+            'null': True,
+        }),
+        ('category', ForeignKey, {
+            'null': True,
+            'blank': False,
+        }),
+        ('is_published', BooleanField, {
+            'default': True,
+        }),
+        ('created_at', DateTimeField, {
+            'auto_now_add': True,
+        }),
     ])
 class TestPostModelAttrs(_TestModelAttrs):
 
